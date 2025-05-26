3067cc1ad1fa63b85489445cd18fd533e0748809
Gabriel <131909622+gc0w0@users.noreply.github.com>
2025-05-26 15:13:44 -0300

    Criado os modulos com as heranças pré definidas.

---
+++ Clube_da_Leitura/Compartilhado/TelaBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clube_da_Leitura.Compartilhado
+{
+    internal class TelaBase
+    {
+    }
+}
---
+++ Clube_da_Leitura/ModuloRevista/RepositorioRevista.cs
@@ -0,0 +1,13 @@
+﻿using Clube_da_Leitura.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clube_da_Leitura.ModuloRevista
+{
+    internal class RepositorioRevista : RepositorioBase
+    {
+    }
+}
