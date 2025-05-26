52b1ce426e25e074e88fce5437aba168b0d6c441
Gabriel <131909622+gc0w0@users.noreply.github.com>
2025-05-26 14:57:43 -0300

    Adicionar arquivos de projeto.

---
+++ Clube_da_Leitura/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Clube_da_Leitura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
